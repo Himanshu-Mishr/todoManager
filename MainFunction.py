dd17d12726b936c82614797f4cd325798ebdcab9
Himanshu Mishra <himanshu.m786@gmail.com>
2013-09-01 14:34:20 +0530

    Todo List Manager :-  	As this is first commit, so this script does only simple task. 	like : *asking for option input 	       *adding a task 	       *showing all task

--- MainFunction.py
+++ MainFunction.py
@@ -24,22 +24,64 @@
 
 def main():
 
+
+  
+  # Information for the user : 
   print('''
     \t\t ToDo List Manager :- 
 
-\t         Select the option below :-
-\t         t ) enter a task.
-\t         x ) exit.
+      \t Select the option below :-
+      \t t ) enter a task.
+      \t x ) exit.
 
     ''')
   # This while loop depends on the option that is input.
   # It run infintly and depends on user. 
   while(1):
-    opt = input("\t\t Enter option :");
+
+    # Asking for the option
+    opt = input("Enter option :");
+
+    # creating a  option for exit
     if opt == 'x':
+      print("\nThank You For Using It\n")
       break
 
 
+    '''
+      Concept : why we are opening & closing main file varible here .
+        If we use simple way to print the file 
+        (which is just creating the a new buffer for reading the file) will not
+        work for reading the current inputs.
+      
+        So to get the current input we need close the main file i.e todoFile
+        this is will save the on going operation. 
+        We have to be caution about, when we enter the next task that should be at last.
+    '''
+
+    # For entering task
+    if opt == 't':
+      
+      '''
+      Open --> Collection --> Writing --> Close
+      '''
+
+      # file opening
+      todoFile = open('data.todo','a')
+      
+      # collection task
+      task = input("Enter Task : - ")
+
+      # writing task to file
+      todoFile.write('     | '+task + '\n')
+
+      # closing the file
+      todoFile.close()
+
+      # Printing the whole data.todo file
+      tempBuffer = open('data.todo','r')
+      print(tempBuffer.read())
+      tempBuffer.close()
   return 0
 
 if __name__ == '__main__':
